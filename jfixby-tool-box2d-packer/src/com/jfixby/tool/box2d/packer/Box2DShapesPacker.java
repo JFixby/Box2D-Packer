059c5320c224add2ecbb82697ff9b21d5cd5fa2d
J Fixby <github@jfixby.com>
2016-04-04 16:03:26 +0200

    +RedJson

--- jfixby-tool-box2d-packer/src/com/jfixby/tool/box2d/packer/Box2DShapesPacker.java
+++ jfixby-tool-box2d-packer/src/com/jfixby/tool/box2d/packer/Box2DShapesPacker.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 import org.box2d.r3.gdx.beditor.Box2DEditorShape;
 
 import com.jfixby.cmns.api.assets.AssetID;
-import com.jfixby.cmns.api.collections.Collection;
 import com.jfixby.cmns.api.collections.Collections;
 import com.jfixby.cmns.api.collections.List;
 import com.jfixby.cmns.api.file.File;
-import com.jfixby.cmns.api.json.Json;
 import com.jfixby.cmns.api.log.L;
-import com.jfixby.cmns.api.sys.Sys;
 import com.jfixby.r3.api.resources.StandardPackageFormats;
 import com.jfixby.rana.api.pkg.fs.PackageDescriptor;
+import com.jfixby.red.engine.core.resources.PackageUtils;
 import com.jfixby.redtriplane.fokker.assets.Box2DEditorProject;
 
 public class Box2DShapesPacker {
@@ -49,30 +47,10 @@ public class Box2DShapesPacker {
 
 	L.d("writing", package_folder);
 
-	producePackageDescriptor(package_folder, StandardPackageFormats.Box2DEditor.Project, "1.0", provisions,
+	PackageUtils.producePackageDescriptor(package_folder, StandardPackageFormats.Box2DEditor.Project, "1.0", provisions,
 		Collections.newList(), input_file.getName());
 
     }
 
-    static private void producePackageDescriptor(File output_folder, String format, String version,
-	    Collection<AssetID> provisions, Collection<AssetID> dependencies, String root_file_name)
-	    throws IOException {
-
-	PackageDescriptor descriptor = new PackageDescriptor();
-	descriptor.format = format;
-	descriptor.timestamp = "" + Sys.SystemTime().currentTimeMillis();
-	descriptor.version = version;
-	for (AssetID d : provisions) {
-	    descriptor.packed_assets.addElement(d.toString());
-	}
-	for (AssetID d : dependencies) {
-	    descriptor.package_dependencies.addElement(d.toString());
-	}
-
-	descriptor.root_file_name = root_file_name;
-	File output_file = output_folder.child(PackageDescriptor.PACKAGE_DESCRIPTOR_FILE_NAME);
-
-	output_file.writeString(Json.serializeToString(descriptor));
-
-    }
+  
 }
